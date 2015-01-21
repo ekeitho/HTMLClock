06db43fe3d458dff628ea74e6d83bfbfadffd4a0
Keith Abdulla <email@ekeitho.com>
2015-01-21 00:27:57 -0800

    finish up clock

--- js/clock.js
+++ js/clock.js
@@ -3,16 +3,17 @@ $(document).ready(function() {
   setTimeout();
 });
 
+var options = {
+  hour: "2-digit", minute: "2-digit", second : "2-digit"
+};
 
 function getTime() {
     var date = new Date();
-    $(".clock").append("HELLO");
-    $(".clock").html("<p> " + date.getHours() + ":" +
-    date.getMinutes() + ":" + date.getSeconds() + "</p>");
+    $(".clock").html("<p> " + date.toLocaleTimeString("en-us", options) + "</p>");
 }
 
 function setTimeout() {
   setInterval(function() {
     getTime()
-  }, 5000);
+  }, 1000);
 }
