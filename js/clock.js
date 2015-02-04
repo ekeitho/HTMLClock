5c9c827a327f3020f2a64a8b2f3bac5dd35b168d
Keith Abdulla <email@ekeitho.com>
2015-02-03 21:59:23 -0800

    update clock with time intervals with soundcloud player

--- js/clock.js
+++ js/clock.js
@@ -80,12 +80,12 @@ function fireAlarm() {
 
       setTimeout(function() {
          alert("Alarm clock has gone off!!");
-         var SC = SC.initialize({
+         var ic = SC.initialize({
             client_id: '82f31c2903e5032ae74bc5c17d82d63c'
          });
 
          var track_url = 'https://soundcloud.com/forss/flickermood';
-         SC.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
+         ic.oEmbed(track_url, { auto_play: true }, function(oEmbed) {
             console.log('oEmbed response: ' + oEmbed);
          });
 
