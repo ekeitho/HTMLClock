e20fd66415b613032fac785a19c69ffbe9f008b1
ekeitho <email@ekeitho.com>
2015-02-19 10:10:42 -0800

    fixing jquery issues

--- js/fb.js
+++ js/fb.js
@@ -1,27 +1,19 @@
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
     console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
+
+    /* on successful log in */
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
         testAPI();
         $('.button').css('display', 'initial');
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
-    } else {
-        // The person is not logged into Facebook, so we're not sure if
-        // they are logged into this app or not.
-        document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
-            
         $('.button').css('display', 'none');
-        console.log("here");
+    } else {
+        /* on log out */
+        /* hide the button */
+        $('.button').css('display', 'none');
     }
 }
 
